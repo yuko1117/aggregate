000acd3b493d8ae3292337f118c1d93a13795213
yuko1117 <eyonseat@yahoo.co.jp>
2020-12-11 05:53:01 +0900

    検索機能

--- app/controllers/aggregate_controller.rb
+++ app/controllers/aggregate_controller.rb
@@ -1,5 +1,26 @@
 class AggregateController < ApplicationController
+  before_action :move_to_index, except: [:index, :show, :search]
+
   def index
+    @items = Item.includes(:user)
+  end
+
+  def search
+    @items = Item.search(params[:keyword])
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :area)
+  end
+
+  def prototype_params
+    params.require(:item).permit(:title, :category_id, :explanation, :image).merge(user_id: current_user.id)
   end
 
 end
